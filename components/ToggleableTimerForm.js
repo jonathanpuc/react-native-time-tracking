7bea87d337f372fe662fcb9beddbb3f836c94117
Jonathan Pucm <jonathancoder77@outlook.com>
2017-12-27 13:26:21 +1100

    flesh out components

--- components/ToggleableTimerForm.js
+++ components/ToggleableTimerForm.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
-import TimerButton from './components/TimerButton';
+import TimerButton from './TimerButton';
 import TimerForm from './TimerForm';
 
 export default function ToggleableTimerForm({ isOpen }) {
